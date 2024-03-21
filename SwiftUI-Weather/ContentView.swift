7e59cbc1748a56a4808265e6e0c53ce3749a10be
Umair Abdus Salam <umairabdsalam@gmail.com>
2024-03-21 22:33:19 +0000

    Initial Commit

---
+++ SwiftUI-Weather/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI-Weather
+//
+//  Created by Umair Salam on 3/21/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
